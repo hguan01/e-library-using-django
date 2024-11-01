b811f244d494f345e7324c379d408a2610fbb75e
Hang-Guan <guanhang0502@gmail.com>
2024-10-31 22:53:08 -0400

    pass CatalogueModelTests

--- tdd_django/elibrary_app/tests.py
+++ tdd_django/elibrary_app/tests.py
@@ -1,7 +1,18 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import TestCase, SimpleTestCase
 from .models import Catalogue
+from django.urls import reverse
+from django.urls.base import resolve
+from .views import home
+
+class ElibraryURLsTest(SimpleTestCase):
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse(home))
+        self.assertEqual(response.status_code, 200)
+
+    def test_root_url_resolves_to_homepage_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
 
 class CatalogueModelTests(TestCase):
 
